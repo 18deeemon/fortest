cda226fc09fcaaddbaa654709cbde6da55924ed9
Dmitri <18deeemon@gmail.com>
2022-04-26 02:45:06 +0300

    Update userdata-1.sh

--- userdata-1.sh
+++ userdata-1.sh
@@ -2,5 +2,8 @@
 sudo su
 apt update -y
 apt install haproxy -y
+service haproxy stop
+sleep 10s
 git clone https://github.com/18deeemon/fortest.git
-mv fortest/haproxy.cfg /etc/haproxy/haproxy.cfg 
+mv fortest/haproxy.cfg /etc/haproxy/haproxy.cfg
+service haproxy start
